f4eded24bab6930a9339df142118d6cbce93d5f5
chirag <sharmachirag242004@gmail.com>
2025-09-21 20:01:35 +0530

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  
+  return (
+    <div>
+      hello web socket
+    </div>
+  )
+}
+
+export default App;
--- socketServer/src/index.ts
+++ socketServer/src/index.ts
@@ -1,21 +1,28 @@
 // we are creating our own websocket server
 // 2 lines of code start the websocket server
 
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
 wss.on('connection', (socket) => {
+    allSockets.push(socket);
+
     console.log('user connected');
     userCount = userCount + 1;
     console.log(`user count is ${userCount}`);
 
     socket.on('message', (message) => {
         console.log('message received: ' + message.toString());
-        setTimeout(() => {
-            socket.send(message.toString() + " sent from the server");
-        }, 2000);
+        
+
+        // iterating on socket array and brodcasting message to all the clients that are connected
+        for(let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s?.send(message.toString() + ": Sent from server");
+        }
     })
 });
